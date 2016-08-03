bf02a4c1e272488616d27bb772d929076a5a2451
Matt Rice <mattrice12@outlook.com>
2016-08-02 22:35:20 -0500

    added user_id to wine table

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   has_secure_password
 
   has_many :userwines, :dependent => :destroy
+  has_many :wines
 
   validates :username, uniqueness: true, length: { minimum: 1, maximum: 30 }
 
---
+++ db/migrate/20160803033053_add_wine_to_users.rb
@@ -0,0 +1,5 @@
+class AddWineToUsers < ActiveRecord::Migration[5.0]
+  def change
+    add_reference :wines, :user, index: true, foreign_key: true
+  end
+end
