56e617f99ccaa991991f7013016bf610021ce496
Matt Rice <mattrice12@outlook.com>
2016-08-03 02:36:51 -0500

    can no longer favorite duplicate items

--- app/controllers/userwines_controller.rb
+++ app/controllers/userwines_controller.rb
@@ -3,7 +3,7 @@ class UserwinesController < ApplicationController
   end
 
   def create
-    Userwine.create!(user_id: session[:user_id], wine_id: params["user_id"])
+    Userwine.find_or_create_by(user_id: session[:user_id], wine_id: params["user_id"])
   end
 
   def destroy
