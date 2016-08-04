9b183fba80b1be56e71fe14d75b0bc6f43069450
Matt Rice <mattrice12@outlook.com>
2016-08-04 16:55:18 -0500

    ITS BROKEN BUT DANE WONT LET ME WORK ON IT ANYMORE

--- app/assets/javascripts/components/wines.js.jsx
+++ app/assets/javascripts/components/wines.js.jsx
@@ -11,7 +11,13 @@ var Wines = React.createClass({
               <div key={wine.id} className="name-index">
                 <div className="blocky-block">
                   <a className="name-index" href={"/wines/" + wine.id}>{wine.name}</a>
-                  <a className="favorite btn" rel="nofollow" data-method="post" href={"/userwines/"}>Favorite</a>
+                  <a className="favorite btn" rel="nofollow" data-method="post" href={"/userwines?" + user_id= [user.id]}>Favorite</a>
+
+
+
+<%= link_to "favorite", userwines_path(user_id: wine.id), method: :post%>
+
+
 
                   <small className="edit-delete-block">
                     <a className="waves-effect waves-light edit-btn btn" rel="nofollow" href={"/wines/" + wine.id + "/edit"}>Edit</a>
